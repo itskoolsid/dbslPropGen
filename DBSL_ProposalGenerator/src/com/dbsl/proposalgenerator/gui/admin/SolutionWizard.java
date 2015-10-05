78e28bc1bfd4b5222b958b9fbe1e2e5111521a26
sidharth.balan <sidharth.balan.x@visualiq.com>
2015-10-05 20:59:15 +0530

    Adress demo added

--- DBSL_ProposalGenerator/src/com/dbsl/proposalgenerator/gui/admin/SolutionWizard.java
+++ DBSL_ProposalGenerator/src/com/dbsl/proposalgenerator/gui/admin/SolutionWizard.java
@@ -8,56 +8,54 @@ import org.vaadin.teemu.wizards.event.WizardStepActivationEvent;
 import org.vaadin.teemu.wizards.event.WizardStepSetChangedEvent;
 
 import com.dbsl.proposalgenerator.beans.Solution;
-import com.dbsl.proposalgenerator.gui.admin.wizard.solution.AddHardwareStep;
-import com.dbsl.proposalgenerator.gui.admin.wizard.solution.AddMonthlyCostStep;
-import com.dbsl.proposalgenerator.gui.admin.wizard.solution.AddSolutionStep;
-import com.dbsl.proposalgenerator.gui.admin.wizard.solution.AddSubCatagoryStep;
+import com.vaadin.data.fieldgroup.FieldGroup;
+import com.vaadin.data.util.BeanItem;
 import com.vaadin.server.Page;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.VerticalLayout;
 
-public class SolutionWizard extends VerticalLayout implements
-		WizardProgressListener {
+public class SolutionWizard extends VerticalLayout implements WizardProgressListener {
 
-	Wizard wizard;
+    Wizard wizard;
 
-	public SolutionWizard() {
-		// TODO Auto-generated constructor stub
-		wizard = new Wizard();
-		wizard.setUriFragmentEnabled(false);
-		wizard.addListener(this);
-		Solution solution = new Solution();
-		wizard.addStep(new AddSolutionStep(solFieldGroup), "empDetails");
-		wizard.addStep(new AddSubCatagoryStep(conBinder), "empContact");
-		wizard.addStep(new AddMonthlyCostStep(addressBinder), "empAddress");
-		wizard.addStep(new AddHardwareStep(privilegeBinder, wizard),
-				"empPrivilege");
-		wizard.setSizeFull();
-		addComponent(wizard);
-		setComponentAlignment(wizard, Alignment.TOP_CENTER);
-	}
+    public SolutionWizard() {
+        // TODO Auto-generated constructor stub
+        wizard = new Wizard();
+        wizard.setUriFragmentEnabled(false);
+        wizard.addListener(this);
+        Solution solution = new Solution();
+        FieldGroup solBinder = new FieldGroup(new BeanItem<Solution>(solution));
+        /*
+         * wizard.addStep(new AddSolutionStep(solBinder), "empDetails"); wizard.addStep(new
+         * AddExecutionSummeryStep(addressBinder)); wizard.addStep(new AddSubCatagoryStep(), "empContact"); wizard.addStep(new
+         * AddMonthlyCostStep(), "empAddress"); wizard.addStep(new AddHardwareStep(), "empPrivilege");
+         */
+        wizard.setSizeFull();
+        addComponent(wizard);
+        setComponentAlignment(wizard, Alignment.TOP_CENTER);
+    }
 
-	@Override
-	public void activeStepChanged(WizardStepActivationEvent event) {
-		Page.getCurrent().setTitle(event.getActivatedStep().getCaption());
-	}
+    @Override
+    public void activeStepChanged(WizardStepActivationEvent event) {
+        Page.getCurrent().setTitle(event.getActivatedStep().getCaption());
+    }
 
-	@Override
-	public void stepSetChanged(WizardStepSetChangedEvent event) {
-		// TODO Auto-generated method stub
+    @Override
+    public void stepSetChanged(WizardStepSetChangedEvent event) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void wizardCompleted(WizardCompletedEvent event) {
-		// TODO Auto-generated method stub
+    @Override
+    public void wizardCompleted(WizardCompletedEvent event) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void wizardCancelled(WizardCancelledEvent event) {
-		// TODO Auto-generated method stub
+    @Override
+    public void wizardCancelled(WizardCancelledEvent event) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
--- DBSL_ProposalGenerator/src/com/dbsl/proposalgenerator/gui/admin/wizard/solution/SolutionAddForm.java
+++ DBSL_ProposalGenerator/src/com/dbsl/proposalgenerator/gui/admin/wizard/solution/SolutionAddForm.java
@@ -11,24 +11,23 @@ import com.vaadin.ui.TextField;
 @SuppressWarnings("serial")
 public class SolutionAddForm extends CustomComponent {
 
-	private TextField name;
-	private TextField motto;
-	private TextField logoPath;
-	private DateField createdOn;
-	private ImageUploadPanel upload;
+    private final TextField name;
+    private final TextField motto;
+    private final DateField createdOn;
+    private final ImageUploadPanel upload;
 
-	public SolutionAddForm() {
-		name = new TextField("Solution Name");
-		motto = new TextField("Solution Motto");
-		createdOn = new DateField("Created On", new Date());
-		upload = new ImageUploadPanel();
+    public SolutionAddForm() {
+        name = new TextField("Solution Name");
+        motto = new TextField("Solution Motto");
+        createdOn = new DateField("Created On", new Date());
+        upload = new ImageUploadPanel();
 
-		name.setNullRepresentation("");
-		motto.setNullRepresentation("");
-		createdOn.setEnabled(false);
+        name.setNullRepresentation("");
+        motto.setNullRepresentation("");
+        createdOn.setEnabled(false);
 
-		FormLayout form = new FormLayout(name, motto, createdOn, upload);
-		setCompositionRoot(form);
-	}
+        FormLayout form = new FormLayout(name, motto, createdOn, upload);
+        setCompositionRoot(form);
+    }
 
 }
